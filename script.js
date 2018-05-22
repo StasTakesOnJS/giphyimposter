982d0f0031617ea8d34d153bcf11071ba2535e08
Stas P <stas.polivyanenko@gmail.com>
2018-05-22 09:37:54 -0700

    Initial commit - the functionality is all there, just commiting to Git

---
+++ script.js
@@ -0,0 +1,91 @@
+const Search = {
+  params: {
+    "api_key": "vx1wCbzp9wKDWaSs3CUBwQ2KfZxbiF9y",
+    "q": "",
+    "limit": 24,
+    "offset": 0
+  },
+
+  gifs: [],
+  url: "",
+
+  //Prepare search URL and increase the offset value
+  prepareURL: function() {
+    if ($("#searchInput").val() == "") {
+      this.params.q = "Trending";
+    } else {
+      let searchString = $("#searchInput").val().split("");
+      searchString.forEach((char, i) => {
+        if (char == ' ') {searchString[i] = '+'}
+      });
+      this.params.q = searchString.join("");
+    };
+
+    Search.url = `http://api.giphy.com/v1/gifs/search?q=${this.params.q}&api_key=${this.params.api_key}&limit=${this.params.limit}&offset=${this.params.offset}`;
+    this.params.offset += this.params.limit;
+  },
+
+  //Run the search using URL prepared by prepareURL method
+  // and render the new Gifs with renderGifs() method
+  search: function() {
+    this.prepareURL();
+    $.ajax({url: this.url, success: (result) => {
+      this.gifs = result.data;
+      this.renderGifs();
+    }});
+  },
+
+  //Clearn the page of current GIFs and run the search() method with the current search string
+  //This is used when clicking the Fetch! button or pressing enter while in the input field
+  handleSearch: function() {
+    $("#results").empty();
+    this.params.offset = 0;
+    this.search();
+  },
+
+  //Render all GIFs from the current this.gifs array of GIF objects
+  renderGifs: function() {
+    for (let i=0;i<this.gifs.length;i++) {
+      let a = $("<a></a>").attr({
+        href: this.gifs[i].images.original.url,
+        target: "_blank"
+      });
+      let g = $("<img></img>").attr({
+        src: this.gifs[i].images.fixed_width_downsampled.url,
+        class: "gifThumbnail",
+        height: 250,
+        width: 250
+      });
+      a.append(g);
+      $("#results").append(a);
+    }
+  }
+}
+
+let input = $("<input></input>").attr({
+  "id":"searchInput"
+}).on({
+  keypress: function(e) {
+    if (e.which == 13) {
+      Search.handleSearch();
+    }
+  }
+});
+
+let searchButton = $("<button id=\"searchButton\"></button>").text("Fetch!").on({
+  click: function() {
+    Search.handleSearch();
+  }
+});
+
+let loadMoreButton = $("<button id=\"loadMoreButton\"></button>").text("Load More GIFs").on("click",() => {
+    Search.search();
+  });
+
+let form = $("<div id=\"inputForm\"></div>").append(input,searchButton);
+
+$("body").append(form,"<br>","<div id=\"results\"></div>","<br>",loadMoreButton);
+
+//Render the trending GIFs when loading the app
+Search.prepareURL();
+Search.search();
